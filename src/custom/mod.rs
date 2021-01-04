702f7dc0de7c5f4da9da22018be4b71afe027045
Texpert-GD <65292910+Texpert-GD@users.noreply.github.com>
2021-01-04 16:36:12 -0500

    Allowed jabs to be canceled into anything.

--- src/custom/mod.rs
+++ src/custom/mod.rs
@@ -18,6 +18,12 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
             ATTACK(ID=0, Part=0, Bone=hash40("rot"), Damage=100.0, Angle=88, KBG=66, FKB=0, BKB=100, Size=8.0, X=0.0, Y=0.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_OFF, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_flower"), SFXLevel=ATTACK_SOUND_LEVEL_L, SFXType=COLLISION_SOUND_ATTR_BAT, Type=ATTACK_REGION_BODY)
             });
         }
+        if StatusModule::status_kind(module_accessor) == *FIGHTER_STATUS_KIND_ATTACK{
+            CancelModule::enable_cancel(module_accessor);
+        }
+        if StatusModule::status_kind(module_accessor) == *FIGHTER_STATUS_KIND_ATTACK_100{
+            CancelModule::enable_cancel(module_accessor);
+        }
 
     }
 }
