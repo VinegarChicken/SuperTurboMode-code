f240c00ad03c25488673a1b594267760a783e781
Texpert-GD <65292910+Texpert-GD@users.noreply.github.com>
2021-01-16 18:52:06 -0500

    Commented out grounded tech, will circle back to it later.

--- src/custom/mod.rs
+++ src/custom/mod.rs
@@ -1,13 +1,13 @@
 use smash::app::lua_bind::*;
-use smash::app::utility::get_kind;
 use smash::lib::lua_const::*;
 use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
 use smash::hash40;
 use smash::lib::lua_const::*;
 use smash::*;
 use acmd::{acmd, acmd_func};
-use smash::cpp::root::app::lua_bind::StatusModule::*;
+use smash::app::lua_bind::StatusModule::*;
 use smash::params::*;
+use smash::cpp::root::app::ItemKind;
 
 // Use this for general per-frame fighter-level hooks
 pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
@@ -19,6 +19,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         //status kind 34 is air dodging
         //status kind 32 is dodging
         let lua_state = fighter.lua_state_agent;
+        let rand_val = app::sv_math::rand(hash40("fighter"), 100);
         let module_accessor = smash::app::sv_system::battle_object_module_accessor(fighter.lua_state_agent);
         let fighter_kind = smash::app::utility::get_kind(module_accessor);
 
@@ -28,6 +29,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         if status_kind(module_accessor) == *FIGHTER_STATUS_KIND_ATTACK_100{
             CancelModule::enable_cancel(module_accessor);
         }
+        /*
         if status_kind(module_accessor) == 103{
            change_status_request_from_script(module_accessor, 83, true);
         }
@@ -37,8 +39,11 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         if StatusModule::status_kind(module_accessor) == 34{
             CancelModule::enable_cancel(module_accessor);
         }
-
-        //println!("{}",StatusModule::status_kind(module_accessor) );
+         */
+        if fighter_kind == *FIGHTER_KIND_DAISY{
+            //ItemModule::have_item(module_accessor,ItemKind(*ITEM_KIND_BOMBHEI),0,0,false,false);
+        }
+        println!("{}",StatusModule::status_kind(module_accessor) );
     }
 }
 
