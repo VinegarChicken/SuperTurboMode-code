ddd6dee98e5ade5abcb0a7f1d6b04e4ca6c3f150
Texpert-GD <65292910+Texpert-GD@users.noreply.github.com>
2021-01-20 22:00:21 -0500

    Mario instant uair, dair, and fair !

--- src/mario/mod.rs
+++ src/mario/mod.rs
@@ -11,7 +11,7 @@ use acmd::{acmd, acmd_func};
 pub fn mario_fair(fighter: &mut L2CFighterCommon) {
     acmd!({
     if(is_execute){
-        MotionModule::set_rate(15.0)
+            MotionModule::set_rate(15.0)
     }
     frame(Frame=3)
     if(is_excute){
@@ -98,10 +98,43 @@ pub fn mario_n_special(fighter: &mut L2CFighterCommon) {
     });
 
 }
+#[acmd_func(
+battle_object_category = BATTLE_OBJECT_CATEGORY_FIGHTER,
+battle_object_kind = FIGHTER_KIND_MARIO,
+animation = "attack_air_hi",
+animcmd = "game_attackairhi")]
+pub fn mario_uair(fighter: &mut L2CFighterCommon) {
+    acmd!({
+    if(is_execute){
+    MotionModule::set_rate(4.0)
+    }
+    frame(Frame=2)
+    if(is_excute){
+        WorkModule::on_flag(Flag=FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING)
+    }
+    frame(Frame=5)
+    if(is_excute){
+        MotionModule::set_rate(1.0)
+        ATTACK(ID=0, Part=0, Bone=hash40("legr"), Damage=7.0, Angle=75, KBG=135, FKB=0, BKB=10, Size=4.4, X=1.2, Y=0.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_KICK)
+        ATTACK(ID=1, Part=0, Bone=hash40("kneer"), Damage=7.0, Angle=75, KBG=135, FKB=0, BKB=10, Size=5.5, X=3.8, Y=0.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=1.0, SDI=1.0, Clang_Rebound=ATTACK_SETOFF_KIND_ON, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_M, SFXType=COLLISION_SOUND_ATTR_KICK, Type=ATTACK_REGION_KICK)
+    }
+    frame(Frame=9)
+    if(is_excute){
+        AttackModule::clear_all()
+    }
+    frame(Frame=18)
+    if(is_excute){
+        WorkModule::off_flag(Flag=FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING)
+    }
+    });
+
+
+}
 pub fn install() {
     acmd::add_hooks!(
         mario_fair,
         mario_dair,
-        mario_n_special
+        mario_n_special,
+        mario_uair
     );
 }
